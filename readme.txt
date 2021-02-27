5a23d6f085c30cecea4d086768127cf7914e282b
Joseph Neathawk <11905983+knighthawk0811@users.noreply.github.com>
2021-02-26 18:30:00 -0600

    update to jquery 3.5.1

--- readme.txt
+++ readme.txt
@@ -1,10 +1,10 @@
-=== VI: Components jQuery ===
+=== VI: Components jQuery 3 ===
 Contributors: Knighthawk
 Donate link:
 Tags:
 Requires at least: ?
 Tested up to: 5.3
-Version: 9.1.191212
+Version: 9.2.200903
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl.html
 
@@ -16,6 +16,9 @@ Just include the jQuery Components, on the public facing side of the website, no
 
 
 == Changelog ==
+
+= 9.2.200903 =
+* update to jQuery version 3.5.1
 
 = 9.1.191212 =
 * FPL v9
--- vi_components_jquery.php
+++ vi_components_jquery.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: VI: Components jQuery
+Plugin Name: VI: Components jQuery 3
 Plugin URI: http://neathawk.us
 Description: Just include the jQuery Components, on the public facing side of the website, nothing more
-Version: 9.1.191212
+Version: 9.2.200903
 Author: Joseph Neathawk
 Author URI: http://Neathawk.com
 License: GNU General Public License v2 or later
@@ -17,17 +17,17 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Add jQuery components
  * wp_enqueue_script( string $handle, string $src = '', array $deps = array(), string|bool|null $ver = false, bool $in_footer = false )
  *
- * @link https://developers.google.com/speed/libraries/
+ * @link https://developers.google.com/speed/libraries/#jquery
  * @link https://developer.wordpress.org/reference/functions/wp_enqueue_script/
  * @link https://developer.wordpress.org/reference/functions/wp_enqueue_style/
- * @version 9.1.191212
+ * @version 9.2.200903
  * @since 9.1.191212
  */
-if ( ! function_exists( 'version_8_load_jquery_components' ) ) :
-function version_8_load_jquery_components() {
+if ( ! function_exists( 'vi_load_jquery_components' ) ) :
+function vi_load_jquery_components() {
     wp_deregister_script('jquery');
-    wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js', array(), '3.4.1', false);
+    wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js', array(), '3.5.1', false);
     //wp_enqueue_script('jquery');
 }
-add_action( 'wp_enqueue_scripts', 'version_8_load_jquery_components' );
+add_action( 'wp_enqueue_scripts', 'vi_load_jquery_components' );
 endif;
